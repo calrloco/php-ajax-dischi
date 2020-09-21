d8c5ab7940c4cb81334e7adb799613c1997f6d9f
carlo briganti <505641nl@saeinstitute.edu>
2020-09-21 14:26:49 +0200

    funxione encode per non ripetere codice

--- database/server.php
+++ database/server.php
@@ -1,19 +1,21 @@
 <?php
 include 'db.php';
 richiestaDb($database);
+function encode($db){
+    header('Content-Type: application/json');
+    echo json_encode($db);
+}
 /// funzione per richesta db di autore
 function richiestaDb($database){
     
     if (empty($_GET['author'])) {
-        header('Content-Type: application/json');
-        echo json_encode($database);
+        encode($database);
     } else {
         foreach ($database as $key) {
             $selectArtist = [];
             if (in_array($_GET['author'], $key)) {
                 array_push($selectArtist, $key);
-                header('Content-Type: application/json');
-                echo json_encode($selectArtist);
+                encode($selectArtist);
             }
         }
     }
